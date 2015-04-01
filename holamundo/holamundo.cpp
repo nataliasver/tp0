bb964e269584d05f04e971c032b7ced269234719
nataliasver <brujinat@gmail.com>
2015-03-31 23:49:54 -0300

    Se agrega el .cpp de hola mundo

---
+++ holamundo/holamundo.cpp
@@ -0,0 +1,18 @@
+/*
+* Autores:
+* Natalia S. Verdura 137.626-3
+* Ultima fecha de modificación: 31/03/2015
+* Titulo: Hola mundo
+*/
+
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hola mundo!" << endl;
+	return 0;
+}
+
